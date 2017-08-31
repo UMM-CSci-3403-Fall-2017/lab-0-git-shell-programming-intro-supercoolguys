8810f5e78a374c9e6de5fc85ac53e9881be9b43d
Philip Blaskowski <blask017@morris.umn.edu>
2017-08-30 22:43:40 -0500

    modify big_clean.sh so it can files and create a tarball of the directory. 8/9 tests passed

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 tar -xf $1 -C $2
-
+home=$(pwd)
 name=`echo "$1" | cut -d'.' -f1`
-
 cd $2/$name
-
+grep -r "DELETE ME!" | cut -d':' -f1 | xargs rm
+cd $home
+tar -czf cleaned_$name.tgz $2/$name
