4ec543957a6acc77ab085b5bfacb70db0e5c79cf
Philip Blaskowski <blask017@morris.umn.edu>
2017-08-30 22:52:52 -0500

    modified big_clean.sh so that it moves the tarball to a desired location. 9/9 tests pass

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -5,5 +5,8 @@ home=$(pwd)
 name=`echo "$1" | cut -d'.' -f1`
 cd $2/$name
 grep -r "DELETE ME!" | cut -d':' -f1 | xargs rm
-cd $home
-tar -czf cleaned_$name.tgz $2/$name
+cd ..
+here=$(pwd)
+tar -czf cleaned_$name.tgz $name
+mv $here/cleaned_$name.tgz $home
+
